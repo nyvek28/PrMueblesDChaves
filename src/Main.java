454cd60a0e959844ddd7e85013e05692583124f1
nyvek28 <kdvlpmnt@gmail.com>
2015-12-06 18:38:51 -0600

    Record de prueba

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,7 @@ public class Main {
 		String msj;
 		g.inicializarPrograma();
 		
-		v = g.registrarDistribuidor("Carrion", "San Jose", 22375454, 12);
-		if(v != null){
-			msj = "Nombre: " + v.get("nombre") + "\n"
-				+ "Direccion: " + v.get("direccion") + "\n"
-				+ "Porcentaje: " + v.get("porcentaje") + "\n";
-		}else{
-			msj = "No se pudo registrar al distribuidor";
-		}
 		
-		System.out.println(msj);
-		
-		a = (new MultiDistribuidor()).listar();
-		
-		for(int i = 0; i < a.size(); i++){
-			System.out.println("id: "+a.get(i).getId()+"\n");
-		}
 		
 	}
 
