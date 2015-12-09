6801659ad05e3bde83abca051e91fd094be909b6
Garritosk8 <emonteror@ucenfotc.ac.cr>
2015-12-09 01:44:10 -0600

    UI hola mundo

--- src/UI.java
+++ src/UI.java
@@ -1,9 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class UI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		motor();
 
+	}
+	
+	private static void motor(){
+		
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				
+				JFrame guiFrame = new JFrame("hello world");
+				guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+				guiFrame.setTitle("Example GUI"); 
+				guiFrame.setSize(300,250); 
+				guiFrame.setLocationRelativeTo(null);
+				guiFrame.setVisible(true);
+				
+			}
+		});
 	}
 
 }
