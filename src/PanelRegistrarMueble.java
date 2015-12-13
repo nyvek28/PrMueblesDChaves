0cb500fce2799bb1de33d0876f015faf4cace81c
dani023 <dani.chaves023@gmail.com>
2015-12-13 15:12:02 -0600

    panelRegistrarMueble

---
+++ src/PanelRegistrarMueble.java
@@ -0,0 +1,29 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.TreeMap;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PanelRegistrarMueble extends PanelFormaMueble {
+
+	private JTextField textFieldID;
+	private JTextField textFieldLinea;
+	private JTextField textFieldAncho;
+	private JTextField textFieldAlto;
+	private JTextField textFieldLargo;
+	private JTextField textFieldCategoria;
+	private JTextField textFieldPrecio;
+	
+	
+	public PanelRegistrarMueble(){
+		
+		super();
+		
+	}
+}
