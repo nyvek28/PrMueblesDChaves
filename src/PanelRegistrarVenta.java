4166a6691b6147a0a56daef1922a9df0c1773449
dani023 <dani.chaves023@gmail.com>
2015-12-16 04:02:46 -0600

    Panel Registrar Venta

---
+++ src/PanelRegistrarVenta.java
@@ -0,0 +1,26 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.TreeMap;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class PanelRegistrarVenta extends PanelFormaVenta {
+
+	private JTextField textFieldID;
+	private JTextField textFieldJuego;
+	private JTextField textFieldIdDistribuidor;
+	private JTextField textFieldMonto;
+
+	
+	public PanelRegistrarVenta(){
+		
+		super();
+		
+	}
+}
