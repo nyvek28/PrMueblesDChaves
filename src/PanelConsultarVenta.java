3d6645f8637841205f5e95b589a30fdf0f493538
dani023 <dani.chaves023@gmail.com>
2015-12-16 03:49:40 -0600

    panel consultar venta y forma venta

--- src/PanelConsultarVenta.java
+++ src/PanelConsultarVenta.java
@@ -1,4 +1,60 @@
+import java.awt.GridBagConstraints;
 
-public class PanelConsultarVenta {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+public class PanelConsultarVenta extends PanelFormaVenta{
+	
+	private JTextField txtId;
+	private JButton btnBuscar;
+	private JLabel lblID;
+	
+	public PanelConsultarVenta(){
+		
+		super();
+		GridBagConstraints c = new GridBagConstraints();
+		
+		this.setLblID(new JLabel("Digite un Id:"));
+		c.gridx = 1;
+		c.gridy = 0;
+		this.add(this.getLblID(), c);
+		
+		this.setTxtId(new JTextField(8));
+		c.gridx++;
+		this.add(this.getTxtId(), c);
+		
+		this.setBtnBuscar(new JButton("Buscar"));
+		c.gridx++;
+		this.add(this.getBtnBuscar(), c);
+		
+		this.getTextFieldID().setEditable(false);
+		
+	}
+	
+	public JTextField getTxtId() {
+		return txtId;
+	}
+
+	public void setTxtId(JTextField txtId) {
+		this.txtId = txtId;
+	}
+	
+
+	public JButton getBtnBuscar() {
+		return btnBuscar;
+	}
+
+	public void setBtnBuscar(JButton btnBuscar) {
+		this.btnBuscar = btnBuscar;
+	}
+
+	public JLabel getLblID() {
+		return lblID;
+	}
+
+	public void setLblID(JLabel lblID) {
+		this.lblID = lblID;
+	}
+	
 }
--- src/PanelFormaVenta.java
+++ src/PanelFormaVenta.java
@@ -1,4 +1,144 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PanelFormaVenta {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class PanelFormaVenta extends JPanel {
+
+	private JTextField textFieldID;
+	private JTextField textFieldJuego;
+	private JTextField textFieldIdDistribuidor;
+	private JTextField textFieldMonto;
+	private JButton btnAceptar;
+	private JButton btnCancelar;
+	
+	public PanelFormaVenta(){
+		
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		JLabel lblID = new JLabel("ID:");
+		c.gridy = 1;
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_END;
+		this.add(lblID,c);
+		
+		JLabel lblLinea = new JLabel("Linea:");
+		c.gridy++;
+		this.add(lblLinea,c);
+		
+		JLabel lblAncho = new JLabel("Ancho:");
+		c.gridy++;
+		this.add(lblAncho,c);
+		
+		JLabel lblAlto = new JLabel("Alto:");
+		c.gridy++;
+		this.add(lblAlto,c);
+		
+		JLabel lblLargo = new JLabel("Largo:");
+		c.gridy++;
+		this.add(lblLargo,c);
+		
+		
+		JLabel lblCategoria = new JLabel("Categoria :");
+		c.gridy++;
+		this.add(lblCategoria,c);
+		
+		JLabel lblPrecio = new JLabel("Precio:");
+		c.gridy++;
+		this.add(lblPrecio,c);
+		
+		textFieldID = new JTextField(8);
+		c.gridy = 1;
+		c.gridx = 2;
+		c.anchor = GridBagConstraints.LINE_START;
+		this.add(textFieldID,c);
+		//textFieldNombre.setColumns(10);
+		
+		textFieldJuego = new JTextField(8);
+		c.gridy++;
+		this.add(textFieldJuego,c);
+		//textFieldApellido.setColumns(10);
+		
+		textFieldIdDistribuidor = new JTextField(8);
+		c.gridy++;
+		this.add(textFieldIdDistribuidor,c);
+		//textFieldTelefono.setColumns(10);
+		
+		textFieldMonto = new JTextField(8);
+		c.gridy++;
+		this.add(textFieldMonto,c);
+		//textFieldTelefono.setColumns(10);
+		
+		
+		
+		
+		btnAceptar = new JButton("Aceptar");
+		c.gridy = 8;
+		c.gridx = 5;
+		this.add(btnAceptar,c);
+		
+		btnCancelar = new JButton("Cancelar");
+		c.gridx = 4;
+		this.add(btnCancelar,c);
+		
+		this.setVisible(true);
+		
+	}
+
+	public JTextField getTextFieldID() {
+		return textFieldID;
+	}
+
+	public void setTextFieldID(JTextField textFieldID) {
+		this.textFieldID = textFieldID;
+	}
+
+
+	public JTextField getTextFieldJuego() {
+		return textFieldJuego;
+	}
+
+	public void setTextFieldJuego(JTextField textFieldJuego) {
+		this.textFieldJuego = textFieldJuego;
+	}
+	
+	public JTextField getTextFieldAlto() {
+		return textFieldIdDistribuidor;
+	}
+
+	public void setTextFieldIdDistribuidor(JTextField textFieldIdDistribuidor) {
+		this.textFieldIdDistribuidor = textFieldIdDistribuidor;
+	}
+	
+	public JTextField getTextFieldMonto() {
+		return textFieldMonto;
+	}
+
+	public void setTextFieldMonto(JTextField textFieldMonto) {
+		this.textFieldMonto = textFieldMonto;
+	}
+
+
+	public JButton getBtnAceptar() {
+		return btnAceptar;
+	}
+
+	public void setBtnAceptar(JButton btnAceptar) {
+		this.btnAceptar = btnAceptar;
+	}
+
+	public JButton getBtnCancelar() {
+		return btnCancelar;
+	}
+
+	public void setBtnCancelar(JButton btnCancelar) {
+		this.btnCancelar = btnCancelar;
+	}
+	
 }
