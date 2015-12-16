e063105e16765b1a13d053bca9a7871ae594c449
Garritosk8 <emonteror@ucenfotc.ac.cr>
2015-12-16 17:45:07 -0600

    cambios y diagrama

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.TreeMap;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -11,6 +13,8 @@ public class Main {
 		for(int i = 6; i <= 9; i++){
 			ids.add(i);
 		}
+		
+		menu();
 		
 //		datos = (new Gestor()).venderJuego(1, 26, 9);
 //		if(datos != null){
@@ -32,5 +36,98 @@ public class Main {
 		
 		
 	}
+	
+	// inicia metodo que se encarga de desplegar el menu
+
+    public static void menu(){
+
+    	String menu = "MENU \n 1.  Armar juego. \n 2.  Realizar venta. \n";
+    	int opcion = 0;  	
+
+        // evaluamos si hay error
+
+        try{
+
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+        }catch(NumberFormatException ex){
+
+            ingresarNumero();
+            menu();
+        }
+
+    	// se le envia la opcion ingresada al switch  del menu
+
+    	switchMenu(opcion);
+    	menu();
+
+    }
+
+    // inicia metodo que se encarga de procesar la opcion ingresada por el usuario
+
+    public static void switchMenu(int opcion){
+
+
+    	switch(opcion){
+
+    		case 1:
+
+
+    			armarJuego();
+    			break;
+
+    		case 2:
+
+    			// se valida si esta en ejecucion el juego o no;
+
+    			realizarVenta();
+    			break;
+
+
+    		default:
+
+    			incorrecto();
+    			break;
+
+    	}
+    }
+    
+    public static void armarJuego(){
+    	
+    	ArrayList<TreeMap<String,String>> lista = null;
+    	try {
+			lista = ((new Gestor()).listarMontadores());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	obtenerMontador(lista);
+    }
+    
+    public static void obtenerMontador(ArrayList<TreeMap<String,String>> lista){
+    	
+    	
+    }
+    
+    public void realizarJuego(){
+    	
+    	
+    }
+    
+ // inicia metodo que imprime error de tipo de dato ingresado
+
+    public static void ingresarNumero(){
+
+        JOptionPane.showMessageDialog(null, "Por favor ingrese unicamente numeros");
+    }
+
+    // inicia metodo que imprime opcion incorrecta
+
+    public static void incorrecto(){
+
+        JOptionPane.showMessageDialog(null, "Opcion incorrecta");
+
+    }
 
 }
